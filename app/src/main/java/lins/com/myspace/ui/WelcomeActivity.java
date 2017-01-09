8d254740e98ee1bc2b6737c767c0257e4f3dcdf8
cyber <753392431@qq.com>
2017-01-09 11:57:32 +0800

    一些bug的完善

--- app/src/main/java/lins/com/myspace/ui/WelcomeActivity.java
+++ app/src/main/java/lins/com/myspace/ui/WelcomeActivity.java
@@ -54,7 +54,6 @@ public class WelcomeActivity extends MyBaseActivity {
             openActivity(MainActivity.class);
             finish();
         }
-        
         @Override
         public void onAnimationRepeat(Animation animation) {
             //当动画重复的时候执行
